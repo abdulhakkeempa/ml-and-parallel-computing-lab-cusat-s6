ffbc1eb0f35488dcaa65971ab20aaff33410caa5
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-02-09 11:27:21 +0530

    Update sequential-n-numbers-sum.cpp

--- sequential-n-numbers-sum.cpp
+++ sequential-n-numbers-sum.cpp
@@ -8,29 +8,36 @@ int sumArray(int *random_array, int N);
 
 int main() {
     int N;
-    cout<< "N : ";
-    cin>>N;
+    cout << "N : ";
+    cin >> N;
 
     int randomArray[N];
 
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         randomArray[i] = rand() % 100;
     }
 
-    int sum;
-    
-    sum = sumArray(&randomArray, N);
-    cout<<sum;
+    // Measure execution time
+    clock_t start = clock();
+
+    int sum = sumArray(randomArray, N);
+
+    clock_t end = clock();
+    double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Execution Time: " << elapsedTime << " seconds" << endl;
+
+    return 0;
 }
 
-
-int sumArray(int *random_array, int N){
-    int sum=0;
+int sumArray(int *random_array, int N) {
+    int sum = 0;
 
     for (int i = 0; i < N; i++) {
-        cout<<random_array[i]<<", ";
+        cout << random_array[i] << ", ";
         sum = sum + random_array[i];
     }
 
