18aa5cbac5ad188b5e6529bb0da41f9beafc1ea8
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-02-15 13:16:41 +0530

    Update parallel-n-numbers.cpp

--- parallel-n-numbers.cpp
+++ parallel-n-numbers.cpp
@@ -4,48 +4,50 @@
 #include <numeric> 
 #include <ctime>
 
-const int arraySize = 1000;
-const int numThreads = 10;
-const int chunkSize = arraySize / numThreads;
-
 void processChunk(const int* array, int startIdx, int endIdx, int& partialSum) {
     partialSum = std::accumulate(array + startIdx, array + endIdx, 0);
 }
 
 int main() {
-    int myArray[arraySize];
-
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    for (int i = 0; i < arraySize; i++) {
-        myArray[i] = rand() % 1000;
+    std::vector<int> sizes = {100, 500, 1000, 2000, 5000, 10000};
+
+    for (int N : sizes) {
+        int* myArray = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            myArray[i] = rand() % 1000;
+        }
+
+        for (int numThreads = 1; numThreads <= 10; ++numThreads) {
+            int chunkSize = N / numThreads;
+
+            clock_t start = clock();
+
+            std::vector<std::thread> threads;
+            std::vector<int> partialSums(numThreads, 0);
+
+            for (int i = 0; i < numThreads; ++i) {
+                int startIdx = i * chunkSize;
+                int endIdx = (i == numThreads - 1) ? N : (i + 1) * chunkSize;
+
+                threads.emplace_back(processChunk, myArray, startIdx, endIdx, std::ref(partialSums[i]));
+            }
+
+            for (auto& thread : threads) {
+                thread.join();
+            }
+
+            int totalSum = std::accumulate(partialSums.begin(), partialSums.end(), 0);
+
+            clock_t end = clock();
+            double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+            std::cout << "Array Size: " << N << ", Number of Threads: " << numThreads << ", Elapsed Time: " << elapsedTime << " seconds, Total Sum: " << totalSum << std::endl;
+        }
+
+        delete[] myArray;
     }
-    
-    // Measure execution time
-    clock_t start = clock();
-
-    
-    std::vector<std::thread> threads;
-    std::vector<int> partialSums(numThreads, 0);
-
-    for (int i = 0; i < numThreads; ++i) {
-        int startIdx = i * chunkSize;
-        int endIdx = (i == numThreads - 1) ? arraySize : (i + 1) * chunkSize;
-
-        threads.emplace_back(processChunk, myArray, startIdx, endIdx, std::ref(partialSums[i]));
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    int totalSum = std::accumulate(partialSums.begin(), partialSums.end(), 0);
-
-    clock_t end = clock();
-    double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-    std::cout<<"Elapsed Time: "<<elapsedTime<<std::endl;
-    
-    std::cout << "Total Sum: " << totalSum << std::endl;
 
     return 0;
 }
