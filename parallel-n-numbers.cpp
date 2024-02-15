9b3b742613e9aa0b79ed45898f162dc4944cefd3
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-02-15 13:32:53 +0530

    Update parallel-n-numbers.cpp

--- parallel-n-numbers.cpp
+++ parallel-n-numbers.cpp
@@ -8,10 +8,22 @@ void processChunk(const int* array, int startIdx, int endIdx, int& partialSum) {
     partialSum = std::accumulate(array + startIdx, array + endIdx, 0);
 }
 
+void printArray(double array[6][10], int n_row, int n_col){
+    for(int a = 0; a < n_row; a++){
+        for(int b = 0; b < n_col; b++){
+          std::cout << array[a][b] << " ";
+        }
+        std::cout << std::endl;
+      }   
+}
+
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
     std::vector<int> sizes = {100, 500, 1000, 2000, 5000, 10000};
+
+    double size_thread_comparison [6][10];
+    int size_thread_comparison_row_idx = 0;
 
     for (int N : sizes) {
         int* myArray = new int[N];
@@ -44,10 +56,13 @@ int main() {
             clock_t end = clock();
             double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
             std::cout << "Array Size: " << N << ", Number of Threads: " << numThreads << ", Elapsed Time: " << elapsedTime << " seconds, Total Sum: " << totalSum << std::endl;
+            size_thread_comparison[size_thread_comparison_row_idx][numThreads-1] = elapsedTime;
         }
-
         delete[] myArray;
+        size_thread_comparison_row_idx++;
     }
-
+    
+    printArray(size_thread_comparison, 6, 10);
+    
     return 0;
 }
